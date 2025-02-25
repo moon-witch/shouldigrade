240c30efa407beb276767be2a1dc5f47c9a337b8
moonwitch <joshua_develops@proton.me>
2025-02-25 17:59:39 +0100

    added pokemon tcg api and test data display

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      PKMN_API_KEY: process.env.PKMN_API
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
+import pokemon from 'pokemontcgsdk'
 
+const config = useRuntimeConfig()
+
+const PKMN_API = config.PKMN_API_KEY
+
+pokemon.configure({apiKey: PKMN_API})
+
+const test = ref("")
+
+const getTestData = async () => {
+  try {
+    const data = await pokemon.card.where({ q: 'name:blastoise', pageSize: 10, page: 3 })
+    test.value = data.data[0].cardmarket.prices
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  getTestData()
+})
 </script>
 
 <template>
-  <div>INDEX</div>
+  <div>
+    <div>TEST aREa</div>
+    <pre>{{ test }}</pre>
+  </div>
 </template>
 
 <style scoped lang="scss">
